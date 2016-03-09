92c871d18d59c12b89597d4ce67240579362fef9
Michel Frechette <mifrec@hotmail.com>
2016-03-09 16:17:34 -0500

    zero base index {0}, \n newline

--- index.php
+++ index.php
@@ -22,22 +22,13 @@ $location = $city . " " . $province;
     <h2><?php echo $location ?></h2>
 
     <section>
-      <pre>
-        <?php
-          $one = 1.0;
-          $two = 2;
-          $three = 3;
-          $string_one = "1";
-          $distance_to_home = 1.2;
-          $distance_to_school = 2.5;
-         ?>
-      </pre>
-      <ul>
-        <li><?php echo gettype($one); ?></li>
-        <li><?php echo gettype($string_one); ?></li>
-        <li><?php echo gettype($one + $two); ?></li>
-        <li><?php echo $distance_to_school + $distance_to_home; ?></li>
-      </ul>
+      <pre><?php
+        $greeting = "Hello Friends!\n";
+        $greeting{0} = "J";
+        $secondary_greeting = "How are you today?\n";
+        echo $greeting;
+        echo $secondary_greeting;
+      ?></pre>
     </section>
   </body>
 </html>
