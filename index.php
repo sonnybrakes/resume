44d431e5d747f9465f7403b9f9921ba494999833
Michel Frechette <mifrec@hotmail.com>
2016-03-09 15:59:35 -0500

    floats, doubles

--- index.php
+++ index.php
@@ -24,16 +24,19 @@ $location = $city . " " . $province;
     <section>
       <pre>
         <?php
-          $one = 1;
+          $one = 1.0;
           $two = 2;
           $three = 3;
           $string_one = "1";
+          $distance_to_home = 1.2;
+          $distance_to_school = 2.5;
          ?>
       </pre>
       <ul>
         <li><?php echo gettype($one); ?></li>
         <li><?php echo gettype($string_one); ?></li>
-        <li><?php echo $one + $two; ?></li>
+        <li><?php echo gettype($one + $two); ?></li>
+        <li><?php echo gettype($distance_to_school + $distance_to_home); ?></li>
       </ul>
     </section>
   </body>
